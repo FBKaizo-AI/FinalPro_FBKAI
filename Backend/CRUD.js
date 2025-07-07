721af0cfe23fbecc40cd9e6433438de7bc025579
DrCloudz <151997602+DrCloudz@users.noreply.github.com>
2025-07-07 11:55:59 -0500

    Add path module import to CRUD.js for backend management and set up static file structure for easier API routing and page rendering

--- Backend/CRUD.js
+++ Backend/CRUD.js
@@ -1,6 +1,7 @@
 //CRUD ops for backend management with MongoDB
 const mongoose = require('mongoose');
 const express = require('express');
+const path = require ('path');
 
 const app = express();
 app.use(express.json());
